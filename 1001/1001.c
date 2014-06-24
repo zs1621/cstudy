f0b877daf602140896c67766feb10793f5fb0d6e
zs1621 <zs1213yh@gmail.com>
2014-06-24 23:59:07 +0800

    continue 1001

--- 1001/1001.c
+++ 1001/1001.c
@@ -3,17 +3,47 @@
 #include <stdlib.h>
 
 void main () {
-    float a[6];
+    char a[6][6];
+    char kill[6][5];
     int b[6];
-    char c[100];
-    int i;
+    int c[6];
+    int i,j, k;
     for ( i = 0; i < 6; i++ ) {
-        scanf("%f", &a[i]);
-        scanf("%d", &b[i]);
+        scanf("%6s %3d", a[i], &b[i]);
     }
     for ( i = 0; i < 6; i++ ) {
-        snprintf(c, "%f", pow(a[i], b[i]);
+        k = 5;
+        for (j = 0; j < 6; j++) {
+            k -= 1;
+            if ( a[i][j] == '.' ) {
+                c[i] = j;
+                k = k + 1;
+            } else {
+                kill[i][k] = a[i][j]; 
+            }
+        } 
     }
-    printf("%s", c);
+    for ( i = 0; i < 6; i++) {
+        int s[5];
+        for ( j = 0; j < 5; j++) {
+            printf("%c--", kill[i][j]);
+        }
+    }
 }
+
+int result[6][125];
+
+/************************
+ *                      *
+ *  @param s 求方的值   *
+ *  @param res 结果     *
+ *  @param num 求方次数 *
+ *                      *
+ ************************/
+void mul(int *s, int *res, int num) {
+    //TODO
+
+}
+
+
 
