4e1d1e479723f2ba9033c75fb5b1050fe291cf09
zs <zs1213yh@gmail.com>
2015-03-24 20:39:05 +0800

    [add some study example]

---
+++ learncthehardway/1_chapter/ex5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* this is a document. */
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+    printf(" You are %d miles away. \n", distance);
+    return 0;
+}
---
+++ learncthehardway/1_chapter/test.c
@@ -0,0 +1,9 @@
+//
+//  test.c
+//  
+//
+//  Created by zs on 15-3-18.
+//
+//
+
+#include "test.h"
